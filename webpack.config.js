530d72adeec4459e6ed10bead6ddfb750202549c
ry596384086 <ry596384086@gmail.com>
2021-09-09 18:30:17 +0800

    =feat: 增加postcss-loader & 开发环境不单独抽离css

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,111 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const SimpleProgressWebpackPlugin = require( 'simple-progress-webpack-plugin' )
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const SimpleProgressWebpackPlugin = require("simple-progress-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
 
+// 生产环境才抽离css文件，避免开发环境下修改css后热更新不加载问题
+getStyleLoader = () => {
+  return process.env.NODE_ENV === "development"
+    ? "style-loader"
+    : MiniCssExtractPlugin.loader;
+};
 
 module.exports = {
-  mode:  process.env.NODE_ENV,
+  mode: process.env.NODE_ENV,
 
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html',
+      template: "public/index.html",
     }),
     new SimpleProgressWebpackPlugin(),
     // new BundleAnalyzerPlugin({analyzerPort: 8080, generateStatsFile: false  })
     new MiniCssExtractPlugin({
-      filename: 'css/[name]-[contenthash].css'
+      filename: "css/[name]-[contenthash].css",
     }),
   ],
 
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
 
   output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
     clean: true,
   },
 
   devServer: {
     hot: true,
     open: true,
-    host: 'localhost',
+    host: "localhost",
     port: 8080,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
-        pathRewrite: { '^/api': ''}
-      }
-    }
+        pathRewrite: { "^/api": "" },
+      },
+    },
   },
 
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      "@": path.resolve(__dirname, "src"),
     },
-    extensions: ['.ts', '.tsx', '.js', 'config.js', '.json'],
+    extensions: [".ts", ".tsx", ".js", "config.js", ".json"],
   },
 
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
-        enforce: 'pre',
-        use: ['babel-loader','ts-loader',{
-          loader: 'eslint-loader',
-          options: { fix: true }}
+        enforce: "pre",
+        use: [
+          "babel-loader",
+          "ts-loader",
+          {
+            loader: "eslint-loader",
+            options: { fix: true },
+          },
         ],
-        include: path.resolve(__dirname, 'src'),
-        exclude: /node_modules/
-      }, 
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+      },
       {
         test: /\.(css|less)$/i,
-        use: [MiniCssExtractPlugin.loader, {
-          loader:  'css-loader',
-          // options: {
-          //   modules: {
-          //     localIdentName: '[name]__[local]-[hash:base64:5]'
-          //   },
-          // }
-        }, 'less-loader']
+        use: [
+          getStyleLoader(),
+          {
+            loader: "css-loader",
+            // options: {
+            //   modules: {
+            //     localIdentName: '[name]__[local]-[hash:base64:5]'
+            //   },
+            // }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins:  ['postcss-preset-env'],
+              }
+            },
+          },
+          "less-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset',
+        type: "asset",
         // parser: {
         //   dataUrlCondition: {
         //     maxSize: 8 * 1024 //小于8kb使用dataUri
         //   }
         // }
       },
-    ]
+    ],
   },
 
   // resolveLoader: {
@@ -97,10 +120,10 @@ module.exports = {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-          },
+          name: "vendors",
+          chunks: "all",
         },
-    }
-  }
-}
+      },
+    },
+  },
+};
